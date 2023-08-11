241d12d7beea1c189c76b064deb52790008d9ef8
Lucas <lucasmanzanel@gmail.com>
2023-08-11 19:34:31 -0300

    diseño de login

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    routeBack: 'https://api.sandbox.pagos360.com'
+    routeGET360: 'https://api.sandbox.pagos360.com',
+    apiKey: 'NjQwNDMxNGI1YzU0YjllYmVhYjJiZDdmY2E5Y2EyMDg5ZDVlODFmNzRmMDc1OGJmMDY2OTY0NzlhNGJiZWQwNA'
 };
